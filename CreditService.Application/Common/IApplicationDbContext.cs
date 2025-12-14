71d7cf7e37e9d4324645b9681d8610bc3729ba17
Dmitry3Lyalyuk <165943906+Dmitry3Lyalyuk@users.noreply.github.com>
2025-12-14 22:45:39 +0300

    Add project files.

---
+++ CreditService.Application/Common/IApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using CredirService.Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace CreditService.Application.Common
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<User> Users { get; }
+        DbSet<Credit> Credits { get; }
+    }
+}
